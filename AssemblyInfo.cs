48a69c3d9893da1b30282c60c9ffcdca80bbd00e
unknown <Dominic.Bassett@mail.bcu.ac.uk>
2025-10-09 14:39:50 +0100

    Assembly Info changed to match GitHub.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("SphericalVoronoiLib")]
-[assembly: AssemblyDescription("C# library for generating spherical Voronoi tessellations and solid angles from SOFA-format coordinate data.")]
+[assembly: AssemblyDescription("C# class library for generating spherical Voronoi tessellations and computing solid angles from SOFA-format or general 3D coordinate data. Compiles as a DLL for easy integration in .NET projects.")]
 [assembly: AssemblyCompany("Birmingham City University")]
 [assembly: AssemblyProduct("SphericalVoronoiLib")]
 [assembly: AssemblyCopyright("MIT License © 2025 Dom Bassett")]
